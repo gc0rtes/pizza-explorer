00757038ea2f346c3b57b1f6ab665080d84b6370
gc0rtes <guilhermeco@gmail.com>
2021-05-28 12:32:19 +0200

    Parameterized Selector done

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,17 +1,31 @@
 //To show our selected state
 import { useSelector } from "react-redux";
 
+//Import useState to store User Interface logic
+import { useState } from "react";
+
 //Import selector from selectors store
-import { selectRestaurants } from "../store/restaurants/selectors";
+// import { selectRestaurants } from "../store/restaurants/selectors"; This was used to sort alphabetically
 import { selectRestaurantsWithPizzas } from "../store/selectors";
+import { selectPizzas } from "../store/pizzas/selectors";
+import { selectRestaurantsThatSellPizza } from "../store/restaurants/selectors";
 
 export default function RestaurantList() {
   //Asign variable to selectors from pizzza store
-  const restaurantList = useSelector(selectRestaurants);
-  console.log("what is restaurantList:", restaurantList);
+  // const restaurantList = useSelector(selectRestaurants);
+  // console.log("what is restaurantList:", restaurantList);
   const restaurantsWithPizzas = useSelector(selectRestaurantsWithPizzas);
-  console.log("what is restaurantsWithPizzas?", restaurantsWithPizzas);
-  console.log("what is ?", restaurantsWithPizzas[0].pizzas[0].name);
+  // console.log("what is restaurantsWithPizzas?", restaurantsWithPizzas);
+  // console.log("what is ?", restaurantsWithPizzas[0].pizzas[0].name);
+
+  const pizzas = useSelector(selectPizzas);
+  console.log("what is pizzas?", pizzas);
+
+  const [selectedPizza, setSelectedPizza] = useState(pizzas[0].id);
+
+  const restaurantsThatSellSelectedPizza = useSelector(
+    selectRestaurantsThatSellPizza(selectedPizza)
+  );
 
   return (
     <div>
@@ -26,6 +40,27 @@ export default function RestaurantList() {
               ))}
             </ul>
           </li>
+        ))}
+      </ul>
+      <h2>
+        Who sells{" "}
+        <select
+          value={selectedPizza}
+          onChange={(e) => {
+            setSelectedPizza(parseInt(e.target.value));
+          }}
+        >
+          {pizzas.map((pizza) => (
+            <option key={pizza.id} value={pizza.id}>
+              {pizza.name}
+            </option>
+          ))}
+        </select>{" "}
+        ?
+      </h2>
+      <ul>
+        {restaurantsThatSellSelectedPizza.map((restaurant) => (
+          <li key={restaurant.id}>{restaurant.name}</li>
         ))}
       </ul>
     </div>
--- src/store/restaurants/selectors.js
+++ src/store/restaurants/selectors.js
@@ -8,3 +8,18 @@ export const selectRestaurants = (reduxState) => {
     return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
   });
 };
+
+/* `Parameterized Selector` to show the reastaurants that sell specific pizza
+Our parameterized selector is actually a function that returns a function.
+That means we will call these two selectors like this:
+
+const restaurants = useSelector(selectRestaurants)
+const restaurantsThatSellSelectedPizza = useSelector(selectRestaurantsThatSellPizza(selectedPizza))
+
+This selector allows us to change the information we select from the state, whenever we change the selectedPizza in our select tag.
+*/
+export const selectRestaurantsThatSellPizza = (pizzaId) => (reduxState) => {
+  return reduxState.restaurants.allRestaurants.filter((restaurant) =>
+    restaurant.pizzas.includes(pizzaId)
+  );
+};
